d9b2167b2f4e5b17de50d6e6ab2a618bf53db662
Hugo Rodger-Brown <hugo@rodger-brown.com>
2013-05-27 14:26:48 +0100

    Warning message for IE users (SVG not supported).

--- lido.py
+++ lido.py
@@ -1,7 +1,7 @@
 """
 stats.py web app used to generate data for geckoboard.
 """
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 app.debug = True
@@ -9,7 +9,7 @@ app.debug = True
 
 @app.route('/')
 def temp():
-    return render_template('chart.html')
+    return render_template('chart.html', browser=request.user_agent.browser)
 
 
 if __name__ == '__main__':
